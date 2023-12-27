683e52414f4ee5e56f1c9199337a6df393f880ac
Viet Anh Duong <anh.duong@kyber.network>
2023-12-27 03:36:41 +0000

    Update wrapper script
    Signed-off-by: Viet Anh Duong <anh.duong@kyber.network>

--- docker/llvm/wrapper.sh
+++ docker/llvm/wrapper.sh
@@ -17,19 +17,23 @@ llvm-objcopy)
   ;;
 esac
 
-ARCH=$(uname -m)
-
-case $ARCH in
-i386 | x86_64)
-  BINARY="$BINARY-amd64"
-  ;;
-aarch64)
-  BINARY="$BINARY-arm64"
-  ;;
-*)
-  echo "Unsupported architecture: $ARCH"
-  exit 1
-  ;;
-esac
+if [[ -z "$TARGETARCH" ]]; then
+  TARGETARCH=$(uname -m)
+  case $TARGETARCH in
+  i386 | x86_64)
+    BINARY="$BINARY-amd64"
+    ;;
+  aarch64)
+    BINARY="$BINARY-arm64"
+    ;;
+  *)
+    echo "Unsupported architecture: $TARGETARCH"
+    exit 1
+    ;;
+  esac
+else
+  # For CI or docker build while we can select the architecture
+  BINARY="$BINARY-$TARGETARCH"
+fi
 
 exec "$BINARY" "$@"
